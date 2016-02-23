b8e45303bb73d9e56b1509ec45769dad5e1d2e2e
Paul Williams <will@Narnia.lan>
2016-02-23 20:35:54 +0000

    passed consumer test

--- lib/service_providers/animal_service_client.rb
+++ lib/service_providers/animal_service_client.rb
@@ -1,10 +1,12 @@
 require 'httparty'
+require 'alligator'
 
 class AnimalServiceClient
   include HTTParty
   base_uri 'http://animal-service.com'
 
   def get_alligator
-    # Yet to be implemented because we're doing Test First Development...
+    name = JSON.parse(self.class.get("/alligator").body)['name']
+    Alligator.new(name)
   end
 end
